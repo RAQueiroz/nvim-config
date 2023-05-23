9060eb47745cd2711836028bdde16dfa8f1b3661
Rodrigo Queiroz <queiroz_rodrigo@outlook.com.br>
2023-05-23 15:10:00 -0300

    replace eslint with eslint_d

--- lua/user/mason.lua
+++ lua/user/mason.lua
@@ -1,68 +1,65 @@
 local mason_ok, mason = pcall(require, 'mason')
 if not mason_ok then
-    return
+  return
 end
 
 local mason_lspconfig_ok, mason_lspconfig = pcall(require, 'mason-lspconfig')
 if not mason_lspconfig_ok then
-    return
+  return
 end
 
 local lspconfig_ok, lspconfig = pcall(require, 'lspconfig')
 if not lspconfig_ok then
-    return
+  return
 end
 
 mason.setup {
-    ui = {
-        icons = {
-            package_installed = '✓',
-            package_pending = '➜',
-            package_uninstalled = '✗',
-        },
+  ui = {
+    icons = {
+      package_installed = '✓',
+      package_pending = '➜',
+      package_uninstalled = '✗',
     },
+  },
 }
 
 mason_lspconfig.setup()
 
 local on_attach = function(_, bufnr)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-    -- Mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local bufopts = { noremap = true, silent = true, buffer = bufnr }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-    vim.keymap.set('n', '<space>wl', function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, bufopts)
-    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap = true, silent = true, buffer = bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
 end
 
 lspconfig.lua_ls.setup {
-    on_attach = on_attach,
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { 'vim' },
-            },
-        },
+  on_attach = on_attach,
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { 'vim' },
+      },
     },
+  },
 }
-
-lspconfig.tsserver.setup {
-    on_attach = on_attach,
-}
-
-lspconfig.rust_analyzer.setup {
-    on_attach = on_attach,
-}
+lspconfig.svelte.setup { on_attach = on_attach }
+lspconfig.tsserver.setup { on_attach = on_attach }
+lspconfig.rust_analyzer.setup { on_attach = on_attach }
+lspconfig.cssls.setup { on_attach = on_attach }
+lspconfig.gopls.setup { on_attach = on_attach }
--- lua/user/null-ls.lua
+++ lua/user/null-ls.lua
@@ -1,43 +1,44 @@
 local has_loaded, null_ls = pcall(require, 'null-ls')
 if not has_loaded then
-    return
+  return
 end
 
 local eslint_condition = {
-    condition = function(utils)
-        return utils.root_has_file {
-            'eslint.config.js',
-            '.eslintrc',
-            '.eslintrc.js',
-            '.eslintrc.cjs',
-            '.eslintrc.yaml',
-            '.eslintrc.yml',
-            '.eslintrc.json',
-        }
-    end,
+  condition = function(utils)
+    return utils.root_has_file {
+      'eslint.config.js',
+      '.eslintrc',
+      '.eslintrc.js',
+      '.eslintrc.cjs',
+      '.eslintrc.yaml',
+      '.eslintrc.yml',
+      '.eslintrc.json',
+    }
+  end,
 }
 
 local sources = {
-    null_ls.builtins.formatting.stylua,
-    null_ls.builtins.formatting.rustfmt,
-    null_ls.builtins.formatting.eslint.with(eslint_condition),
-    null_ls.builtins.diagnostics.eslint.with(eslint_condition),
+  null_ls.builtins.formatting.stylua,
+  null_ls.builtins.formatting.gofumpt,
+  null_ls.builtins.formatting.rustfmt,
+  null_ls.builtins.formatting.eslint_d.with(eslint_condition),
+  null_ls.builtins.diagnostics.eslint_d.with(eslint_condition),
 }
 
 local augroup = vim.api.nvim_create_augroup('LspFormatting', { clear = true })
 null_ls.setup {
-    debug = true,
-    on_attach = function(client, bufnr)
-        if client.supports_method 'textDocument/formatting' then
-            vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
-            vim.api.nvim_create_autocmd('BufWritePre', {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    vim.lsp.buf.format { bufnr = bufnr, timeout_ms = 2000 }
-                end,
-            })
-        end
-    end,
-    sources = sources,
+  debug = true,
+  on_attach = function(client, bufnr)
+    if client.supports_method 'textDocument/formatting' then
+      vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format { bufnr = bufnr, timeout_ms = 2000 }
+        end,
+      })
+    end
+  end,
+  sources = sources,
 }
