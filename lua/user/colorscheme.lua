b38b3a7c5d3b70b4c5656f1d106c8c1a03f597d0
Rodrigo Queiroz <queiroz_rodrigo@outlook.com.br>
2023-05-30 11:34:49 -0300

    make tokyo night the default

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,42 +1,42 @@
 vim.cmd [[ colorscheme default ]]
 
 local colorschemes = {
-    tokyo = 'tokyonight',
-    nord = 'onenord',
+  tokyo = 'tokyonight',
+  nord = 'onenord',
 }
 
-local colorscheme = colorschemes.nord
+local colorscheme = colorschemes.tokyo
 
 local status_ok, _ = pcall(vim.cmd, 'colorscheme ' .. colorscheme)
 if not status_ok then
-    vim.notify('colorscheme ' .. colorscheme .. ' not found')
-    return
+  vim.notify('colorscheme ' .. colorscheme .. ' not found')
+  return
 end
 
 if colorscheme == colorschemes.tokyo then
-    vim.g.tokyonight_style = 'storm'
-    vim.cmd [[colorscheme tokyonight]]
+  vim.g.tokyonight_style = 'storm'
+  vim.cmd [[colorscheme tokyonight]]
 end
 
 if colorscheme == colorschemes.nord then
-    require('onenord').setup {
-        theme = 'dark', -- "dark" or "light". Alternatively, remove the option and set vim.o.background instead
-        borders = true, -- Split window borders
-        fade_nc = false, -- Fade non-current windows, making them more distinguishable
-        styles = {
-            comments = 'NONE', -- Style that is applied to comments: see `highlight-args` for options
-            strings = 'NONE', -- Style that is applied to strings: see `highlight-args` for options
-            keywords = 'NONE', -- Style that is applied to keywords: see `highlight-args` for options
-            functions = 'NONE', -- Style that is applied to functions: see `highlight-args` for options
-            variables = 'NONE', -- Style that is applied to variables: see `highlight-args` for options
-            diagnostics = 'underline', -- Style that is applied to diagnostics: see `highlight-args` for options
-        },
-        disable = {
-            background = false, -- Disable setting the background color
-            cursorline = false, -- Disable the cursorline
-            eob_lines = true, -- Hide the end-of-buffer lines
-        },
-        custom_highlights = {}, -- Overwrite default highlight groups
-        custom_colors = {}, -- Overwrite default colors
-    }
+  require('onenord').setup {
+    theme = 'dark',                    -- "dark" or "light". Alternatively, remove the option and set vim.o.background instead
+    borders = true,                    -- Split window borders
+    fade_nc = false,                   -- Fade non-current windows, making them more distinguishable
+    styles = {
+      comments = 'NONE',               -- Style that is applied to comments: see `highlight-args` for options
+      strings = 'NONE',                -- Style that is applied to strings: see `highlight-args` for options
+      keywords = 'NONE',               -- Style that is applied to keywords: see `highlight-args` for options
+      functions = 'NONE',              -- Style that is applied to functions: see `highlight-args` for options
+      variables = 'NONE',              -- Style that is applied to variables: see `highlight-args` for options
+      diagnostics = 'underline',       -- Style that is applied to diagnostics: see `highlight-args` for options
+    },
+    disable = {
+      background = false,       -- Disable setting the background color
+      cursorline = false,       -- Disable the cursorline
+      eob_lines = true,         -- Hide the end-of-buffer lines
+    },
+    custom_highlights = {},     -- Overwrite default highlight groups
+    custom_colors = {},         -- Overwrite default colors
+  }
 end
